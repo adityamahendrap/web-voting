c23257981eb25e8e5815cb2f9b119a1461decb48
adityamahendrap <ptadityamahendrap@gmail.com>
2023-12-22 14:48:13 +0800

    rename title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Musma 2023</title>
+    <title>Musma Teknik 2023</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
